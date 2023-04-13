a1424c2588a10575514742fb96204c77ece89320
Mantvydas Racickas <mantvydas.racickas@gmail.com>
2023-04-13 10:07:01 +0300

    keli pakeitimai

--- pirma_programele.py
+++ pirma_programele.py
@@ -3,9 +3,9 @@ import random
 # zaidimas atspeti skaiciu nuo 1 iki 10
 print("\nAtspek skaiciu nuo 1 iki 10")
 mano_skaicius = int(input("Iveskite savo skaiciu: "))
-ai_skaicius = random.randint(1,10)
+ai_skaicius = random.randint(1, 10)
 if mano_skaicius == ai_skaicius:
-    print("OHO atspejai! >:0, mano skaicius buvo: ", ai_skaicius)
+    print(("oho atspejai! >:0, mano skaicius buvo: ").upper(), ai_skaicius)
 elif mano_skaicius != ai_skaicius:
     print("neatspejai, bandyk dar karta ;) mano skaicius buvo: ", ai_skaicius)
 else:
@@ -39,10 +39,11 @@ zmogeliukai = [
 zmoniu_suma_sarase = len(zmogeliukai)
 komandu_kiekis = 3
 
-while zmoniu_suma_sarase > 0 and komandu_kiekis > 0:
+while zmoniu_suma_sarase > 0:
     komanda = random.sample(zmogeliukai, int(zmoniu_suma_sarase/komandu_kiekis))
     for zmogus in komanda:
         zmogeliukai.remove(zmogus)
     zmoniu_suma_sarase -= int(zmoniu_suma_sarase/komandu_kiekis)
     komandu_kiekis -= 1
     print("\nKomanda Nr.", (3-komandu_kiekis), komanda)
+
