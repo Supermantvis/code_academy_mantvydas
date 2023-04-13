7e5db01aa364359ce07b719640ab8824619a5e02
Mantvydas Racickas <mantvydas.racickas@gmail.com>
2023-04-13 14:36:07 +0300

    patobulintos sarasu01 uzduotys

--- 03_ciklai_sarasai_tuples_aibes_zodynai/01_sarasai.py
+++ 03_ciklai_sarasai_tuples_aibes_zodynai/01_sarasai.py
@@ -34,26 +34,42 @@ Atspausdinkite sąrašo kas antrą elementą.
 
 """
 Ketvirta uzduotis:
-Pridėkite dar vieną mėgstamo maisto pavadinimą į sąrašo galą.+
-Įterpkite naują maisto produktą į sąrašo ketvirtą vietą.+
-Pakeiskite antrąjį sąrašo elementą kitu mėgstamo maisto pavadinimu.+
-Ištrinkite pirmąjį sąrašo elementą.+
-Ištrinkite sąrašo trečią elementą.+
-Suraskite sąraše kurio nors elemento indeksą.+
-Atspausdinkite sąrašą atvirkštine tvarka.+
-Pabandykite papildomai panaudoti kitus metodus.+
+Pridėkite dar vieną mėgstamo maisto pavadinimą į sąrašo galą. 1+ 
+Įterpkite naują maisto produktą į sąrašo ketvirtą vietą. 2+ 
+Pakeiskite antrąjį sąrašo elementą kitu mėgstamo maisto pavadinimu. 3+
+Ištrinkite pirmąjį sąrašo elementą. 4+
+Ištrinkite sąrašo trečią elementą. 5+
+Suraskite sąraše kurio nors elemento indeksą. 6+
+Atspausdinkite sąrašą atvirkštine tvarka. 7+
+Pabandykite papildomai panaudoti kitus metodus. 8+
 """
-# sarasas = ['obuolys', 'bananas', 'kriause', 'melionas', 'alus']
-# sarasas.append('pienas')
-# sarasas.insert(3, 'kivi')
-# sarasas[1] = 'kakava'
-# sarasas.remove('obuolys')
-# sarasas.remove('kriause')
-# print(sarasas.index('alus'))
-# sarasas.reverse()
-# print('issimtas_elementas 2: ', sarasas.pop(3))
-# print(sarasas)
+sarasas = ['obuolys', 'bananas', 'kriause', 'melionas', 'alus']
+print('\npradinis sarasas: ', sarasas)
 
+sarasas.append('pienas') # 1
+print('\n1. uzt. pridetas elementas: \'pienas\':', sarasas)
+
+sarasas.insert(3, 'kivi') # 2
+print('\n2. uzd. iterptas 4 elementas \'kivi\':', sarasas)
+
+sarasas[1] = 'kakava' # 3
+print('\n3. uzd. 2 elementas pakeistas i \'kakava\':', sarasas)
+
+sarasas.remove('obuolys') # 4
+print('\n4. uzd. istrintas 1 elementas:', sarasas)
+
+del sarasas[2] # 5
+print('\n5. uzd. istrintas 3 elementas:', sarasas)
+
+print('\n6. uzd. elemento \'alus\' inexas:', sarasas.index('alus')) # 6
+
+sarasas.reverse() # 7
+print('\n7. uzd. sarasas atvirkstine tvarka:', sarasas)
+
+sarasas.pop(3) # 8
+print('\n8. uzd. is\'pop\'intas 4 elementas', sarasas)
+
+print('\ngalutinis sarasas: ', sarasas)
 
 """
 # Penkta užduotis:
