9e93b4c077fa0d4c9a94853e92cf81c0f7bc615a
Mantvydas Racickas <mantvydas.racickas@gmail.com>
2023-04-14 15:57:50 +0300

    update

--- 03_ciklai_sarasai_tuples_aibes_zodynai/komandinis_darbas_main_app.py
+++ 03_ciklai_sarasai_tuples_aibes_zodynai/komandinis_darbas_main_app.py
@@ -1,37 +1,32 @@
 import os
 
-backlog_list = []
+to_do_list = []
 done_list = []
 # in_progress_list = []
 # in_review_list = []
 
 os.system('cls')
 
-while True:   # PRADINIS PROGRAMOS VEIKIMO LYGIS
+while True:
     os.system('cls')
     print('-------[JIRA KNOCKOFF\'as]----------')
-    print('Pasirinkite 1 jeigu norite dirbti su uzduociu tvarkymu')
-    print('Pasirinkite 2 jeigu norite iseiti is programos')
-
+    print('-------[Uzduociu tvarkytuvas]----------')
+    print('BACKLOG TASKS sarasas: ', to_do_list)
+    print('DONE TASKS sarasas: ', done_list)
+    print('Pasirinkite 1 jeigu norite prideti nauju uzduociu i to do list\'a')
+    print('Pasirinkite 2 jeigu norite perkelti tasks is to_do_list i done_list')
+    print('Pasirinkite 3 jeigu norite perkelti tasks is done_list i to_do_list')
+    print('Pasirinkite 4 jeigu norite perkelti grizti i pagrindini meniu')
     pasirinkimas = input("Pasirinkite: ")
+
+
     if pasirinkimas == "1":
-        while True:  # UZDUOCIU TVARKYMO LYGIS
-            os.system('cls')
-            print('-------[Uzduociu tvarkymas]----------')
-            print('Pasirinkite 1 jeigu norite prideti nauju uzduociu i backlog\'a')
-            print('Pasirinkite 2 jeigu norite perkelti tasks is backlog_list i done_list')
-            print('Pasirinkite 3 jeigu norite perkelti tasks is done_list i backlog_list')
-            print('Pasirinkite 4 jeigu norite perkelti grizti i pagrindini meniu')
-            pasirinkimas = input("Pasirinkite: ")
+        os.system('cls')
+        to_do_list.append(input("Irasykite uzduoti i to do list: "))
+        print('BACKLOG sarasas: ', to_do_list)
+        print('uzduotis irasyta i to do list\'a')
+        print('')
 
-            if pasirinkimas == "1":
-                backlog_list.append(input("Irasykite uzduoti: "))
-                print('BACKLOG sarasas: ', backlog_list)
-
-
-            if pasirinkimas == "2":
-                print('griztame i pagrindini meniu.')
-                break
 
 
     if pasirinkimas == "2":
