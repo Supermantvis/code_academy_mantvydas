59f3922735895892abced94075911ff44041700a
Mantvydas Racickas <mantvydas.racickas@gmail.com>
2023-04-14 15:34:47 +0300

    smol shet

--- 03_ciklai_sarasai_tuples_aibes_zodynai/komandinis_darbas_main_app.py
+++ 03_ciklai_sarasai_tuples_aibes_zodynai/komandinis_darbas_main_app.py
@@ -5,30 +5,35 @@ done_list = []
 # in_progress_list = []
 # in_review_list = []
 
+os.system('cls')
 
-os.system('clear')
-while len(backlog_list) <= 3:
-    print("-------[JIRA KNOCKOFF'as]----------")
-    new_task = input('irasykite nauja uzduoti: ')
-    backlog_list.append(new_task)
+while True:   # PRADINIS PROGRAMOS VEIKIMO LYGIS
+    os.system('cls')
+    print('-------[JIRA KNOCKOFF\'as]----------')
+    print('Pasirinkite 1 jeigu norite dirbti su uzduociu tvarkymu')
+    print('Pasirinkite 2 jeigu norite iseiti is programos')
 
-    print(backlog_list)
-
-
-
-
-
-
-
-while True:
-   
-    print("-------[ Mūsų uber programėlė ]----------")
-    print("1: aidas")
-    print("0: išeiti")
     pasirinkimas = input("Pasirinkite: ")
-    if pasirinkimas == "0":
-        break
     if pasirinkimas == "1":
-        tekstas = input("Šaukite: ")
-        print((tekstas+" ") * 3)
-        input("paspauskite ENTER kad tęsti...")
+        while True:  # UZDUOCIU TVARKYMO LYGIS
+            os.system('cls')
+            print('-------[Uzduociu tvarkymas]----------')
+            print('Pasirinkite 1 jeigu norite prideti nauju uzduociu i backlog\'a')
+            print('Pasirinkite 2 jeigu norite perkelti tasks is backlog_list i done_list')
+            print('Pasirinkite 3 jeigu norite perkelti tasks is done_list i backlog_list')
+            print('Pasirinkite 4 jeigu norite perkelti grizti i pagrindini meniu')
+            pasirinkimas = input("Pasirinkite: ")
+
+            if pasirinkimas == "1":
+                backlog_list.append(input("Irasykite uzduoti: "))
+                print('BACKLOG sarasas: ', backlog_list)
+
+
+            if pasirinkimas == "2":
+                print('griztame i pagrindini meniu.')
+                break
+
+
+    if pasirinkimas == "2":
+        print('iseiname is programos.')
+        break
