b02c2d17d3e90466b4592c1368c0f5a391eab232
Mantvydas Racickas <mantvydas.racickas@gmail.com>
2023-04-17 11:00:18 +0300

    funkciju uzduotys done

---
+++ 04_funkcijos/01_funkcijos_aprasymo_struktura_uzduotys.py
@@ -0,0 +1,56 @@
+import os
+
+
+'''
+Pirma užduotis
+Parašykite funkciją lyginis_nelyginis(skaicius), kuri patikrina, ar skaičius yra lyginis ar nelyginis ir išveda atitinkamą pranešimą.
+'''
+
+# def lyginis_nelyginis(skaicius):
+#     if (skaicius % 2) == 0:
+#         print("Įvestas skaičius yra lyginis.")
+#     elif (skaicius % 2) > 0:
+#         print("Įvestas skaičius yra nelyginis.")
+#     else:
+#         print("Įvestas argumentas nėra skaičius.")
+
+# skaiciaus_tipas(44)
+
+
+'''
+Antra užduotis
+Parašykite funkciją ar_teigiamas(skaicius), kuri patikrina, ar skaičius yra teigiamas ir išveda atitinkamą pranešimą.
+'''
+
+# def ar_teigiamas(skaicius):
+#     if skaicius > 0:
+#         print("Įvestas skaičius yra teigiamas.")
+#     elif skaicius < 0:
+#         print("Įvestas skaičius yra neigiamas.")
+#     elif skaicius == 0:
+#         print("Įvestas skaičius yra lygus 0.")
+#     else:
+#         print("Įvestas argumentas nėra skaičius.")
+
+# ar_teigiamas(0)
+
+
+'''
+Trečia užduotis
+Parašykite funkciją ar_daugiau_nei(skaicius, riba), kuri patikrina, ar skaičius yra didesnis nei nurodyta riba ir išveda atitinkamą pranešimą.
+'''
+
+# def ar_daugiau_nei(skaicius, riba):
+#     os.system('cls')
+#     if skaicius > riba:
+#         print(f"Įvestas skaičius: {skaicius} yra didesnis uz riba: {riba}.")
+#     elif skaicius < riba:
+#         print(f"Įvestas skaičius: {skaicius} yra mazesnis uz riba: {riba}.")
+#     elif skaicius == riba:
+#         print(f"Įvestas skaičius: {skaicius} yra lygus ribai: {riba}.")
+#     else:
+#         print(f"Įvestas argumentas: {skaicius} nėra skaičius.")
+
+# ar_daugiau_nei(40, 20)
+# ar_daugiau_nei(20, 20)
+# ar_daugiau_nei(-40, 20)
